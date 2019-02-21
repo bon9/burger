6405d41e4d95b8473697751396e01a1f056141b9
Oleh <boshaa3@gmail.com>
2019-02-22 00:00:50 +0200

    Improving Performance

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Modal.css';
 import Aux from '../../../hoc/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-	<Aux>
-		<Backdrop
-			show={props.show}
-			clicked={props.modalClosed} />
-		<div
-			className={classes.Modal}
-			style={{
-				transform: props.show ? 'translateY(0)' : 'translateY(-105vh)',
-				opcaity: props.show ? '1' : '0'
-			}}>
-			{props.children}
-		</div>
-	</Aux>
-);
+class Modal extends Component {
 
-export default modal;
+	shouldComponentUpdate(nextProps, nextState) {
+		// если при обновлении обновленный show равен текущему
+		// то вернёт false и компонент не обновится
+		// то есть ререндерим Modal, только если show нового рендера отличается от текущего
+		return nextProps.show !== this.props.show;
+	}
+
+	componentWillUpdate() {
+		console.log('[Modal] WillUpdate');
+	}
+
+	render() {
+		return (
+			<Aux>
+				<Backdrop
+					show={this.props.show}
+					clicked={this.props.modalClosed} />
+				<div
+					className={classes.Modal}
+					style={{
+						transform: this.props.show ? 'translateY(0)' : 'translateY(-105vh)',
+						opcaity: this.props.show ? '1' : '0'
+					}}>
+					{this.props.children}
+				</div>
+			</Aux>
+		);
+	}
+}
+
+export default Modal;
